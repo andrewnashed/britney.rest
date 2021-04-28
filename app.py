252ef2e565dc6b267a4e913693e89a07be0242fd
Andrew <cyberandrew30@yahoo.com>
2021-04-27 23:08:01 -0400

    Adding Requests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response, request
 from flask_restful import Api, Resource
+import requests
 import json
 import random
 
@@ -9,16 +10,17 @@ api = Api(app)
 
 class Britney(Resource):
     def get(self):
-        with open('quotes.json') as data_file:
-            quotes = json.load(data_file)
-            random_quote = random.choice(quotes)
-            if request.args.get("format") == "text":
-                response = make_response(random_quote, 200)
-                response.mimetype = "text/plain"
-                return response
-            return random_quote, 200
+        url = "https://raw.githubusercontent.com/andrewnashed/britney.rest/main/quotes.json"
+        response = requests.get(url)
+        quotes = json.loads(response.content.decode())
+        random_quote = random.choice(quotes)
+        if request.args.get("format") == "text":
+            response = make_response(random_quote, 200)
+            response.mimetype = "text/plain"
+            return response
+        return random_quote, 200
 
 
 api.add_resource(Britney, '/')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
